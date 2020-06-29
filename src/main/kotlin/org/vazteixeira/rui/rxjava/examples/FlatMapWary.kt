1fa299bf9935afaf8193914b42680a03f236da84
takecare <rui@vazteixeira.org>
2020-06-29 08:53:24 +0100

    add example on common mistake with flatmap

---
+++ src/main/kotlin/org/vazteixeira/rui/rxjava/examples/FlatMapWary.kt
@@ -0,0 +1,31 @@
+package org.vazteixeira.rui.rxjava.examples
+
+import io.reactivex.Observable
+import org.vazteixeira.rui.rxjava.Example
+import org.vazteixeira.rui.rxjava.ExampleSchedulers
+import org.vazteixeira.rui.rxjava.runExample
+
+fun main() = runExample(FlatMapWary)
+
+sealed class Result<T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Failure<T>(val error: Throwable) : Result<T>()
+}
+
+object FlatMapWary : Example {
+
+    private fun getSomething() = Observable.error<Result<String>>(Error("boom!"))
+        .onErrorReturn { Result.Failure(it) }
+
+    private fun getSomethingElse() = Observable.just<Result<String>>(Result.Success("something else"))
+
+    override val observable: Observable<Result<String>> =
+        getSomething()
+            .subscribeOn(ExampleSchedulers.computation())
+            .flatMap { getSomethingElse() }
+}
+
+// example output:
+//[main]	onSubscribe
+//[RxComputationThreadPool-1]	onNext: Success(data=something else)
+//[RxComputationThreadPool-1]	onComplete!]
